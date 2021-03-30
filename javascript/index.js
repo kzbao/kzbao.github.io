3a2db139d5ed2d2201e448051f614099e6e77bad
Kevin Bao <kzb@kevinbao.com>
2021-03-30 01:21:26 -0700

    Updated photos, moved files around

---
+++ javascript/index.js
@@ -0,0 +1,19 @@
+(() => {
+  var index = 0;
+  var photos = document.getElementById('photos');
+  var captions = document.getElementById('captions');
+
+  photos.addEventListener('click', () => {
+    rotate();
+  });
+
+  function rotate() {
+    photos.children[index].style.display = 'none';
+    captions.children[index].style.display = 'none';
+
+    index = (index + 1) % photos.childElementCount;
+
+    photos.children[index].style.display = 'block';
+    captions.children[index].style.display = 'block';
+  }
+})()
