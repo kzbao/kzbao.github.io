5a04bc46c43c3d29d0302bae60c216e9ed18d92c
Kevin Bao <kbao@assurance.com>
2022-10-01 19:05:50 -0700

    Added left rotate

--- javascript/index.js
+++ javascript/index.js
@@ -3,15 +3,22 @@
   var photos = document.getElementById('photos');
   var captions = document.getElementById('captions');
 
-  photos.addEventListener('click', () => {
-    rotate();
+  photos.addEventListener('click', (event) => {
+    if (event.x > photos.offsetLeft + photos.clientWidth / 2) {
+      rotate(1);
+    } else {
+      rotate(-1);
+    }
   });
 
-  function rotate() {
+  function rotate(skip) {
     photos.children[index].lastElementChild.style.display = 'none';
     captions.children[index].style.display = 'none';
 
-    index = (index + 1) % photos.childElementCount;
+    index = (index + skip) % photos.childElementCount;
+    if (index == -1) {
+      index = photos.childElementCount - 1;
+    }
 
     photos.children[index].lastElementChild.style.display = 'block';
     captions.children[index].style.display = 'block';
